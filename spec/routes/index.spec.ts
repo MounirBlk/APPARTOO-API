f8614cd9ea3e5a6752fbed5fa2aaeadf7807709b
Mounir <mounir.ballouk@gmail.com>
2022-12-16 02:12:55 +0100

    fix bug

--- spec/routes/index.spec.ts
+++ spec/routes/index.spec.ts
@@ -1,14 +1,12 @@
 import request from "supertest"
-import { convertToFormBody, exist, getTimeout, randNumber, randomChars, randomFileName } from "../helpers";
 import app from "../../app";
-import ScenarioModel from "../../src/models/ScenarioModel";
 
 export const htmlSpec = () => {
     it('Test index.html', (done: DoneFn) => {
         request(app).get('').expect(200, done);
-    }, getTimeout());
+    }, 60000);
 
     it('Test error.html', (done: DoneFn) => {
-        request(app).get(`/${randomChars()}${randNumber(1, 100)}`).expect(404, done);
-    }, getTimeout());
+        request(app).get(`/error`).expect(404, done);
+    }, 60000);
 }
